f99ebf2676b589cb165ecd2da0719f2d248b4212
Brendan K. Krueger <bkkrueger@gmail.com>
2014-04-30 10:23:26 +0200

    Added the ability to handle multiple descriptor files, and made some corrections to the PyPar bits to make it play nicer regardless of whether you launch with or without mpirun (also handles the case that PyPar is not installed).

--- Driver.py
+++ Driver.py
@@ -27,12 +27,32 @@ sys.path.append("/Users/bkrueger/research/CCSNe/heating_layer/results/sample/dum
 import argparse as ap
 import glob
 import tomlpython as toml
-import pypar
 import warnings
 
 import Descriptors as Desc
 from MakeFrames import make_all_frames
 
+#==============================================================================
+# Note on PyPar:
+#    I haven't found much documentation or other information online about this,
+# but it appears that calling certain PyPar functions causes a segmentation
+# fault if this script is launched directly (i.e., "python Driver.py" and not
+# "mpirun -np # python Driver.py").  Fortunately, the size and rank functions
+# work either way.  Because I want this script to be able to run on a single
+# processor (both for users who don't need parallelization and for users who
+# have not installed PyPar), I handle the importing of PyPar as a special case,
+# and all calls to PyPar functions are wrapped in a check that more than one
+# processor exists.
+try:
+   import pypar
+except ImportError:
+   NProcs = 1
+   ProcID = 0
+   warnings.warn("Unable to import PyPar.  Running in single-processor mode.",
+         UserWarning)
+else:
+   NProcs = pypar.size()
+   ProcID = pypar.rank()
 #==============================================================================
 
 def pretty(d, indent=0):
@@ -50,10 +70,6 @@ def pretty(d, indent=0):
 
 if __name__ == "__main__":
 
-   # Parallel setup - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-   NProcs = pypar.size()
-   ProcID = pypar.rank()
-
    # Parse command-line arguments - - - - - - - - - - - - - - - - - - - - - - -
    prog_desc = "Generate a set of movies from DUMSES data files."
    parser = ap.ArgumentParser(description=prog_desc)
@@ -64,13 +80,13 @@ if __name__ == "__main__":
          dest="directory")
 
    # Specify the file describing the movies
-   parser.add_argument("--mfile", metavar="MFILE", default="movies.toml",
-         help="file describing movies to be made", required=False,
-         dest="mfile")
+   parser.add_argument("--dfile", metavar="DFILE", nargs="+",
+         default=["descriptors.toml"], required=False, dest="dfile_list",
+         help="file(s) describing movies and masks")
 
    # Specify which movies to generate
-   parser.add_argument("--movies", nargs="+",
-         help="list of IDs of movies to create", dest="movie_list")
+   parser.add_argument("--movies", metavar="MOVIE", nargs="+",
+         dest="movie_list", help="list of IDs of movies to create")
 
    args = parser.parse_args()
 
@@ -97,12 +113,41 @@ if __name__ == "__main__":
       output_list = pypar.receive(0)
 
    # Get the movie and mask descriptions
-   with open(args.mfile) as tomlfile:
-      descriptions = toml.parse(tomlfile)
+   movie_descriptions = {}
+   movie_blacklist = set()
+   mask_descriptions = {}
+   mask_blacklist = set()
+   for dfile in args.dfile_list:
+      with open(dfile) as tomlfile:
+         d = toml.parse(tomlfile)
+         # Make sure movies are not redundant
+         for k, v in d.get("movies", {}).items():
+            if k in movie_descriptions and k not in movie_blacklist:
+               if ProcID == 0:
+                  msg = '"'.join(('Movie ', k, ' is defined multiple times.  ',
+                     'It will be removed from the list of movies.'))
+                  warnings.warn(msg, UserWarning)
+               movie_blacklist.add(k)
+            else:
+               movie_descriptions[k] = v
+         # Make sure masks are not redundant
+         for k, v in d.get("masks", {}).items():
+            if k in mask_descriptions and k not in mask_blacklist:
+               if ProcID == 0:
+                  msg = '"'.join(('Mask ', k, ' is defined multiple times.  ',
+                     'It will be removed from the list of masks.'))
+                  warnings.warn(msg, UserWarning)
+               mask_blacklist.add(k)
+            else:
+               mask_descriptions[k] = v
+   for k in movie_blacklist:     # Remove redundant movies
+      movie_descriptions.pop(k)
+   for k in mask_blacklist:      # Remove redundant masks
+      mask_descriptions.pop(k)
 
    # Construct the mask descriptors
    masks = {}
-   for name, mask_string in descriptions["masks"].items():
+   for name, mask_string in mask_descriptions.items():
       masks[name] = Desc.MaskDescriptor(mask_string)
 
    # Construct the movie descriptors
@@ -111,14 +156,13 @@ if __name__ == "__main__":
    # -- If the user did not specify a list of movies, make all movies described
    #    in the movies file.
    movies = {}
-   for name, movie_string in descriptions["movies"].items():
+   for name, movie_string in movie_descriptions.items():
       if args.movie_list is None or name in args.movie_list:
          movies[name] = Desc.MovieDescriptor(movie_string, masks)
    if args.movie_list is not None:
       for m in args.movie_list:
          if m not in movies:
-            msg = '"'.join(('Movie ', m, ' not found in file ',
-               args.mfile, '.'))
+            msg = '"'.join(('Requested movie ', m, ' not found.'))
             warnings.warn(msg, UserWarning)
 
    # Summarize masks and movies
@@ -132,6 +176,18 @@ if __name__ == "__main__":
          print "   {0}:".format(name), movie
       print "="*79
 
+   # Summarize outputs list
+   if NProcs > 1:
+      pypar.barrier()
+   for i in xrange(NProcs):
+      if i == ProcID:
+         print "Processor {p} has {n} output files:".format(p=ProcID,
+               n=len(output_list))
+         for o in output_list:
+            print "    {0}".format(o)
+      if NProcs > 1:
+         pypar.barrier()
+
    # Call the primary loop
    make_all_frames(output_list, movies)
 
@@ -139,6 +195,7 @@ if __name__ == "__main__":
    # TODO : For every movie that calls for this step, encode the individual
    # frame images into a movie.
 
-   pypar.barrier()
-   pypar.finalize()
+   if NProcs > 1:
+      pypar.barrier()
+      pypar.finalize()
 
